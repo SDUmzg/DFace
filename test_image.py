4cf4be417e1a04d7c360437c7e2cbd7666b5b026
SDUmzg <sdumzg@gmail.com>
2017-12-23 12:07:31 +0800

    WINDOWS CODES

--- test_image.py
+++ test_image.py
@@ -1,16 +1,19 @@
 import cv2
-from core.detect import create_mtcnn_net, MtcnnDetector
-import core.vision as vision
+from src.core.detect import create_mtcnn_net, MtcnnDetector
+import src.core.vision as vision
 
 
 
 
 if __name__ == '__main__':
-
-    pnet, rnet, onet = create_mtcnn_net(p_model_path="./model_store/pnet_epoch_5best.pt", r_model_path="./model_store/rnet_epoch_1.pt", o_model_path="./model_store/onet_epoch_7bbest.pt", use_cuda=True)
+    p_model_path = "./model_store/pnet_epoch.pt"
+    r_model_path = "./model_store/rnet_epoch.pt"
+    o_model_path = "./model_store/onet_epoch.pt"
+    img_path = "./test.jpg"
+    pnet, rnet, onet = create_mtcnn_net(p_model_path, r_model_path, o_model_path, use_cuda=True)
     mtcnn_detector = MtcnnDetector(pnet=pnet, rnet=rnet, onet=onet, min_face_size=24)
 
-    img = cv2.imread("./test.jpg")
+    img = cv2.imread(img_path)
     b, g, r = cv2.split(img)
     img2 = cv2.merge([r, g, b])
 
